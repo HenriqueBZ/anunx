c2d5ec90a7201f7858be1f3c472352c924bd98b1
Henrique Zanini <hiquebzanini@gmail.com>
2021-12-09 11:33:53 -0300

    adicionando pagina de publicar anuncio

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,11 @@ import { createTheme } from "@material-ui/core/styles"
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#000000'
+            main: '#000000',
+        },
+        background: {
+            default: 'rgb(242, 244, 245)',
+            white: '#ffffff',
         }
     }
 })
