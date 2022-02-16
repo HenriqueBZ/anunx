902c5f3174a95dd6ea7eccfbc170aac6c9f01ba5
Henrique Zanini <hiquebzanini@gmail.com>
2022-02-16 11:05:55 -0300

    integrando pagina de produto com api

--- pages/[category]/[productName]/[id].js
+++ pages/[category]/[productName]/[id].js
@@ -13,7 +13,10 @@ import {
 import { makeStyles } from '@material-ui/core/styles'
 import Carousel from 'react-material-ui-carousel'
 
-import TemplateDefault from '../../src/templates/Default'
+import TemplateDefault from '../../../src/templates/Default'
+import ProductsModel from '../../../src/models/products'
+import dbConnect from '../../../src/utils/dbConnect'
+import { formatCurrency } from '../../../src/utils/currency'
 
 const useStyles = makeStyles((theme) => ({
     box: {
@@ -36,7 +39,7 @@ const useStyles = makeStyles((theme) => ({
     },    
 }))
 
-const Product = () => {
+const Product = ({ product }) => {
     const classes = useStyles()
 
     return (
@@ -55,34 +58,31 @@ const Product = () => {
                                     }
                                 }}
                             >
-                                <Card className={classes.card}>
-                                    <CardMedia
-                                        className={classes.cardMedia} 
-                                        image="https://source.unsplash.com/random?a=1"
-                                        title="Título da Imagem"
-                                    />
-                                </Card>
-                                <Card className={classes.card}>
-                                    <CardMedia
-                                        className={classes.cardMedia} 
-                                        image="https://source.unsplash.com/random?a=2"
-                                        title="Título da Imagem"
-                                    />
-                                </Card>
+                                {
+                                    productfile.map(file => (
+                                        <Card key={file.name} className={classes.card}>
+                                            <CardMedia
+                                                className={classes.cardMedia} 
+                                                image={`/uploads/${file.name}`}
+                                                title={product.title}
+                                            />
+                                        </Card>
+                                    ))
+                                }           
                             </Carousel>
                         </Box>
 
                         <Box className={classes.box} textAlign="left">
-                            <Typography component="span" variant="caption">Publicado em 11 de dezembro 2021</Typography>
-                            <Typography component="h4" variant="h4" className={classes.productName}>Tesla Model Y 2022</Typography>
-                            <Typography component="h4" variant="h4" className={classes.price}>R$ 330.000,00</Typography>
-                            <Chip label="Categoria" />
+                            <Typography component="span" variant="caption">Publicado em 11 de dezembro 2021 -- TO DO</Typography>
+                            <Typography component="h4" variant="h4" className={classes.productName}>{product.title}</Typography>
+                            <Typography component="h4" variant="h4" className={classes.price}>{formatCurrency(product.price)}</Typography>
+                            <Chip label={product.category} />
                         </Box>
 
                         <Box className={classes.box} textAlign="left">                            
                             <Typography component="h6" variant="h6">Descrição</Typography>
                             <Typography component="p" variant="body2">
-                                O Tesla Model Y 2022 oferece a possibilidade de escolher entre 5 ou 7 assentos para passageiros. Com versatilidade máxima, os bancos traseiros se dobram de forma independente, criando um armazenamento flexível para toda bagagem de todos ocupantes.
+                                {product.description}
                             </Typography>                            
                         </Box>
                     </Grid>
@@ -91,25 +91,43 @@ const Product = () => {
                         <Card elevation={0} className={classes.box}>
                             <CardHeader
                                 avatar={
-                                    <Avatar>H</Avatar>
+                                    <Avatar src={product.user.image}>
+                                        { product.user.image || product.user.name[0] }
+                                    </Avatar>
                                 }
-                                title="Henrique Zanini"
-                                subheader="hiquebzanini@gmail.com"
+                                title={product.user.name}
+                                subheader={product.user.email}
                             />
                             <CardMedia
-                                image="'https://source.unsplash.com/random'"
-                                title="Henrique Zanini"
+                                image={product.user.image}
+                                title={product.user.name}
                             />
                         </Card>
 
                         <Box className={classes.box}>
-                            <Typography component="h6" variant="h6">Localização</Typography> 
+                            <Typography component="h6" variant="h6">
+                                Localização -- Desafio
+                            </Typography>
                         </Box>                        
                     </Grid>
                 </Grid>
             </Container>
         </TemplateDefault>
     )
+}
+
+export async function getServerSideProps({ query }) {
+    const { id } = query
+
+    await dbConnect()
+
+    const product = await ProductsModel.findOne({ _id: id })
+    
+    return {
+        props: {
+            product: JSON.parse(JSON.stringify(product))
+        }
+    }
 }
 
 export default Product
